04811cb36d3843ceb3214dd0b29542a64c2a9dc1
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2024-03-30 13:53:09 +0000

    fix(lint): remove unused types

--- sql/cursor.go
+++ sql/cursor.go
@@ -14,10 +14,6 @@ type Cursor struct {
 type tmpTableNode struct {
 	node bptree.Node[uint64, Row]
 }
-type tmpLeaf bptree.LeafNode[uint64, Row]
-type tmpRoot bptree.InternalNode[uint64, Row]
-type tmpKey uint64
-type tmpVal Row
 
 func TableStart(table *Table) (*Cursor, error) {
 	rootNode, err := table.pager.getPage(table.RootPageNum)
